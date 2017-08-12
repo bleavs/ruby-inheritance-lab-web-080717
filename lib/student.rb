cd7ec4746633f529be8f79c821d873318310400f
bleavs <bleavitt@colgate.edu>
2017-08-12 09:48:20 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(str)
+    @knowledge << str #adding string to arr that's initialized
+  end
+
+  def knowledge #acts as a reader for knowledge instance var
+    @knowledge
+  end
 
 end
